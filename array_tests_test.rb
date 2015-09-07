815ca835bfad7ba3a5b8dfa8ce7d203db7fe6e84
josephmckenzie <josephmckenzieaz@yahoo.com>
2015-09-07 17:35:48 -0400

    Update array_tests_test.rb

--- array_tests_test.rb
+++ array_tests_test.rb
@@ -5,7 +5,7 @@ class Array_tests_test< Minitest::Test
 
 def test_first_array_has_no_repeated_values
   results=array_tests
-  assert_equal(nil, results.detect {|e| results.rindex(e) != results.index(e) })
+  assert_equal(nil, results)
 end
 end
 
