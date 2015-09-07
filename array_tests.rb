2bf1d877b1ca2b0365733e0a32626c10924dfe3a
josephmckenzie <josephmckenzieaz@yahoo.com>
2015-09-07 17:40:23 -0400

    Update array_tests.rb
    2nd array to check

--- array_tests.rb
+++ array_tests.rb
@@ -1,7 +1,10 @@
 def array_tests
-
 array=[]
 array.detect{|arr| array.index(arr) != array.rindex(arr) }
-
-
 end
+
+def array_tests2
+array=[1,2,3,4]
+array.detect{|arr| array.index(arr) != array.rindex(arr) }
+end
+
