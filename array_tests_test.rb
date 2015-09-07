28b408edb889663910fe881bf67bedca98c6089c
josephmckenzie <josephmckenzieaz@yahoo.com>
2015-09-07 14:36:10 -0400

    Create array_tests_test.rb
    test to see if array 1 has any multiple values which the array is blank so it will come back nil

---
+++ array_tests_test.rb
@@ -0,0 +1,11 @@
+require_relative"array_tests.rb"
+require"minitest/autorun"
+
+class Array_tests_test< Minitest::Test
+
+def test_first_array_has_no_repeated_values
+  results=array_tests
+  assert_equal(nil, results.detect {|e| results.rindex(e) != results.index(e) })
+end
+end
+
